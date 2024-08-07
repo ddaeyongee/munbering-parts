337e323f7063a2653bd02139449c040a4508e760
taeyong <ddaeyongee@gmail.com>
2024-08-07 14:39:58 +0900

    자릿수 수정

--- src/main/java/com/assignment/numbering_parts/service/GuidService.java
+++ src/main/java/com/assignment/numbering_parts/service/GuidService.java
@@ -11,12 +11,28 @@ public class GuidService {
     private final String serverId;
 
     public GuidService(@Value("${server.id}") String serverId) {
-        this.serverId = serverId;
+        this.serverId = normalizeServerId(serverId);
     }
 
+    private String normalizeServerId(String id) {
+        if (id == null) {
+            id = "";
+        }
+        if (id.length() < 4) {
+            return String.format("%4s", id).replace(' ', '0');
+        } else if (id.length() > 4) {
+            return id.substring(0, 4);
+        }
+        return id;
+    }
+
+    //serverId 4자리
+    //timestamp 17자리
+    //randomSuffix 9자리
+    //4+17+9=30자리
     public String generateGuid() {
         String timestamp = GuidGenerator.generateTimestamp();
-        String randomSuffix = UUID.randomUUID().toString().substring(0, 8).replace("-", "");
-        return String.format("%s-%s-%s", serverId, timestamp, randomSuffix);
+        String randomSuffix = UUID.randomUUID().toString().replaceAll("-", "").substring(0, 9);
+        return String.format("%s%s%s", serverId, timestamp, randomSuffix);
     }
 }
